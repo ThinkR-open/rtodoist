a20b91842f9ea4e77ff9cca25fe6b845f8ab5fce
vincent <vincent@thinkr.fr>
2022-12-27 21:58:25 +0100

    move to httr2

--- R/utils.R
+++ R/utils.R
@@ -22,13 +22,30 @@ random_key <- function() {
 #' @param ... any params of POST request
 #' @param url url to call
 #' @return list
-call_api <- function(...,url= "https://todoist.com/api/v9/sync"){
+call_api <- function(...,url= "https://todoist.com/api/v9/sync",token = get_todoist_api_token()){
   
   message("call_api")
   
-  POST(
-    url,
-    ...)
+  # POST(
+  #   url,
+  #   ...)
+  
+  request(base_url = url) %>% 
+    req_headers(
+      Authorization = glue::glue("Bearer {token}"),
+    ) %>% 
+    # req_body_multipart(sync_token="*", 
+    #                    resource_types='[\"projects\"]') %>%
+    req_body_multipart(...) %>%
+    
+    # req_dry_run()
+    req_perform() %>% 
+    resp_body_json()
+  
+  
+  
+  
+  
 }
 
 #' Call project data
